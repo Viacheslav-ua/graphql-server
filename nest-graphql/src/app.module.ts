29825ee9ffda1908ae9ffaa2651661354a914d19
Viacheslav-ua <75425740+Viacheslav-ua@users.noreply.github.com>
2022-05-17 16:44:28 +0300

    server development completed

--- nest-graphql/src/app.module.ts
+++ nest-graphql/src/app.module.ts
@@ -1,55 +1,38 @@
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import {ConfigModule, ConfigService} from '@nestjs/config'
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { SequelizeModule } from "@nestjs/sequelize"
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: "../.env"
     }),
-    //  TypeOrmModule.forRootAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: async (config: ConfigService) => ({
-    //     // host: 'localhost',
-    //     // ssl: true,
-        
-    //     type: config.get<'aurora-data-api'>('TYPEORM_CONNECTION'),
-    //     username: config.get<string>('TYPEORM_USERNAME'),
-        //  password: config.get<string>('TYPEORM_PASSWORD'),
-    //     // password: '1234567',
-
-    //     database: config.get<string>('TYPEORM_DATABASE'),
-    //     port: Number(config.get<number>('TYPEORM_PORT')),
-    //     entities: [__dirname + 'dist/**/*.entity{.ts, .js}'],
-    //     // entities: ["dist/**/*.entity.js"],
-    //     synchronize: true,
-    //     autoLoadEntities: true,
-    //     logging: true,
-    //     // logger: 'advanced-console'
-        
-    //    })
-    //  })
-    SequelizeModule.forRoot({
-      // dialectOptions: {
-      //   ssl: {
-      //       require: true,
-      //       rejectUnauthorized: false
-      //   }
-      // },
-      // ssl: true,
-      dialect: 'postgres',
-      host: 'localhost',
-      port: Number(process.env.TYPEORM_PORT),
-      username: process.env.TYPEORM_USERNAME,
-      password: process.env.TYPEORM_PASSWORD,
-      database: process.env.TYPEORM_DATABASE,
-      // models: [Note, Category],
-      autoLoadModels: true,
-    })
-    
-   ],
+    GraphQLModule.forRoot({
+      driver: ApolloDriver,
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      playground: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        type: config.get<'aurora-data-api'>('TYPEORM_CONNECTION'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        port: Number(config.get<number>('TYPEORM_PORT')),
+        entities: [__dirname + 'dist/**/*.entity{.ts, .js}'],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true,
+      })
+     }),
+     UsersModule
+  ],
   providers: [],
 })
 export class AppModule {}
